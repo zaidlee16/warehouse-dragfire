6389f00f60487175fa9dee926f48d66d56d8105d
zaidlee16 <zaidzaid1807@gmail.com>
2024-10-23 14:51:06 +0700

    first commit

--- src/utils/SupaClient.js
+++ src/utils/SupaClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_url = VITE_SUPABASE_URL;
-const supabase_key = VITE_SUPABASE_KEY;
+const supabase_url = import.meta.env.VITE_SUPABASE_URL;
+const supabase_key = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabase_url, supabase_key);
