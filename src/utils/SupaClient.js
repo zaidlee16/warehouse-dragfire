5e7ba3c4d30efa7f70228aa9cf61576072186d1e
zaidlee16 <zaidzaid1807@gmail.com>
2024-11-06 11:07:33 +0700

    first commit

--- src/utils/SupaClient.js
+++ src/utils/SupaClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_url = import.meta.env.VITE_SUPABASE_URL;
-const supabase_key = import.meta.env.VITE_SUPABASE_KEY;
+const supabase_url = "https://zaabtxvybtowcxcocnkz.supabase.co";
+const supabase_key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphYWJ0eHZ5YnRvd2N4Y29jbmt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4MTEzMDMsImV4cCI6MjA0MDM4NzMwM30.ImZMHlI0f6WHZP5ZH1XZ3cpavUMhQFHb1ByylYSHpoA";
 
 export const supabase = createClient(supabase_url, supabase_key);
